cd5465f1cfae636f001839784932f957221577d9
KevinZhou <1065438834@qq.com>
2023-03-21 10:04:09 +0800

    cookie配置

---
+++ src/utils/cookies.ts
@@ -0,0 +1,23 @@
+import cookie from "react-cookies";
+
+// 获取cookie
+export const getCookie = (key: string) => {
+  return cookie.load(key);
+};
+// 设置cookie
+export const setCookie = (key: string, value: any) => {
+  const expires = new Date();
+  expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14);
+  cookie.save(key, value, {
+    path: "/",
+    expires,
+    maxAge: 1000,
+    domain: "https://play.bukinoshita.io",
+    secure: true,
+    httpOnly: true,
+  });
+};
+// 删除cookie
+export const removeCookie = (key: string) => {
+  cookie.remove(key);
+};
